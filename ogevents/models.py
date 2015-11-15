753dd5c71ed6ebb10df32d3584d8e34ce0d74430
Harry Stock <harrystock@outlook.com>
2015-11-15 02:24:48 +0000

    15/11/15

--- ogevents/models.py
+++ ogevents/models.py
@@ -1,6 +1,8 @@
 import datetime
-from django.db import models
-from django.utils import timezone
+from django.db                              import models
+from django.utils                           import timezone
+from django.contrib.auth.models             import User
+
 
 class Userdetail(models.Model):
   user                = models.OneToOneField('auth.User')
@@ -11,13 +13,13 @@ class Userdetail(models.Model):
     return str(self.user)
 
 class Event(models.Model):
-  author              = models.ForeignKey(Userdetail)
-  author_name         = models.CharField(max_length=20,default="schememanager")
-  event_date               = models.DateField('Event date in the format yyyy-mm-dd, e.g for the 24th of August 2015, enter 2015-08-24',max_length=10)
-  schememanager       = models.ForeignKey(Userdetail, related_name='post_scheme_manager',blank=True,null=True)
-  event_detail        = models.TextField('Event details')
-  created_date        = models.DateTimeField(default=timezone.now)
-  is_live             = models.BooleanField(default=True)
+  author            = models.ForeignKey(Userdetail)
+  author_name       = models.CharField(max_length=20,default="schememanager")
+  event_date        = models.DateField('Event date in the format yyyy-mm-dd, e.g for the 24th of August 2015, enter 2015-08-24', max_length=10)
+  schememanager     = models.ForeignKey(Userdetail, related_name='post_scheme_manager',blank=True,null=True)
+  event_detail      = models.TextField('Event details')
+  created_date      = models.DateTimeField(default=timezone.now)
+  is_live           = models.BooleanField(default=True)
   def __str__(self):               
     return self.event_detail
   def publish(self):
--- ogevents/views.py
+++ ogevents/views.py
@@ -3,55 +3,55 @@ from django.utils                         import timezone
 from django.contrib.auth.decorators       import login_required
 from django.contrib.auth.models           import User
 from .models                              import Event, Userdetail
-from .forms                               import EventForm
+from .forms                               import EventForm, UserdetailForm
 
 def event_list(request):
-  eventxs                       = Event.objects.filter(is_live=True,event_date__gte=timezone.now()).order_by('event_date')
-  events                        = []
+  eventxs                             = Event.objects.filter(is_live=True,event_date__gte=timezone.now()).order_by('event_date')
+  events                              = []
 
   if request.user.is_authenticated():
-    user                        = User.objects.get(id=request.user.id)
-    userdetail                  = user.userdetail
+    user                              = User.objects.get(id=request.user.id)
+    #userdetail                        = user.userdetail
     for eventx in eventxs:
-      if user.is_superuser      == True:
-        editable                = '1'
-      elif eventx.author         == userdetail:
-        editable                = '1'
-      elif eventx.schememanager  == userdetail:
-        editable                = '1'
+      if user.is_superuser            == True:
+        editable                      = '1'
+      elif eventx.author              == user:
+        editable                      = '1'
+      elif eventx.schememanager       == user:
+        editable                      = '1'
       else:
-        editable                = '0'
-      event                     = [ eventx, editable ]
+        editable                      = '0'
+      event                           = [ eventx, editable ]
       events.append(event)
   else:
     for eventx in eventxs:
-      editable                  = '0'
-      event                     = [ eventx, editable ]   
+      editable                        = '0'
+      event                           = [ eventx, editable ]   
       events.append(event)
   return render(request, 'ogevents/event_list.html', {'events': events, 'period':'1'})
 
 def event_list_past(request):
-  eventxs = Event.objects.filter(is_live=True,event_date__lt=timezone.now()).order_by('-event_date')
-  events                        = []
+  eventxs                             = Event.objects.filter(is_live=True,event_date__lt=timezone.now()).order_by('-event_date')
+  events                              = []
 
   if request.user.is_authenticated():
-    user                        = User.objects.get(id=request.user.id)
-    userdetail                  = user.userdetail
+    user                              = User.objects.get(id=request.user.id)
+    #userdetail                        = user.userdetail
     for eventx in eventxs:
-      if user.is_superuser      == True:
-        editable                = '1'
-      elif eventx.author         == userdetail:
-        editable                = '1'
-      elif eventx.schememanager  == userdetail:
-        editable                = '1'
+      if user.is_superuser            == True:
+        editable                      = '1'
+      elif eventx.author              == user:
+        editable                      = '1'
+      elif eventx.schememanager       == user:
+        editable                      = '1'
       else:
-        editable                = '0'
-      event                     = [ eventx, editable ]
+        editable                      = '0'
+      event                           = [ eventx, editable ]
       events.append(event)
   else:
     for eventx in eventxs:
-      editable                  = '0'
-      event                     = [ eventx, editable ]   
+      editable                        = '0'
+      event                           = [ eventx, editable ]   
       events.append(event)
   return render(request, 'ogevents/event_list.html', {'events': events, 'period':'0'})
 
@@ -67,20 +67,20 @@ def event_new(request):
     if form.is_valid():
       event                      = form.save(commit=False)
       if event.event_date < timezone.localtime(timezone.now()).date():
-        return render(request, 'ogevents/event_edit.html', {'form': form})
+        return render(request, 'ogevents/event_input_form.html', {'form': form})
       user                       = User.objects.get(id=request.user.id)
-      userdetail                 = user.userdetail
+      #userdetail                 = user.userdetail
 
       schememanager              = Userdetail.objects.get(schememanager=userdetail.schememanager,is_schememanager=True)
       event.author_name          = user.username
-      event.author               = userdetail
+      event.author               = user
       event.schememanager        = schememanager
       event.save()
 
       return redirect('ogevents.views.event_list')
   else:
     form = EventForm()
-  return render(request, 'ogevents/event_edit.html', {'form': form})
+  return render(request, 'ogevents/event_input_form.html', {'form': form})
 
 @login_required
 def event_new_past(request):
@@ -90,18 +90,18 @@ def event_new_past(request):
       event                      = form.save(commit=False)
 
       user                       = User.objects.get(id=request.user.id)
-      userdetail                 = user.userdetail
+      #userdetail                 = user.userdetail
 
       schememanager              = Userdetail.objects.get(schememanager=userdetail.schememanager,is_schememanager=True)
       event.author_name          = user.username
-      event.author               = userdetail
+      event.author               = user
       event.schememanager        = schememanager
       event.save()
 
       return redirect('ogevents.views.event_list_past')
   else:
     form = EventForm()
-  return render(request, 'ogevents/event_edit.html', {'form': form})
+  return render(request, 'ogevents/event_input_form.html', {'form': form})
 
 @login_required
 def event_edit(request, period, pk):
@@ -113,19 +113,19 @@ def event_edit(request, period, pk):
       event                      = form.save(commit=False)
 
       user = User.objects.get(id=request.user.id)
-      userdetail                 = user.userdetail
+      #userdetail                 = user.userdetail
 
       if user.is_superuser       == True:
         pass
-      elif event.author          == userdetail:
+      elif event.author          == user:
         pass
-      elif event.schememanager   == userdetail:
+      elif event.schememanager   == user:
         pass
       else:
-        return render(request, 'ogevents/event_edit.html', {'form': form})
+        return render(request, 'ogevents/event_input_form.html', {'form': form})
 
       event.author_name          = user.username
-      event.author               = userdetail
+      event.author               = user
       event.save()
 
       if period == '1':
@@ -134,20 +134,20 @@ def event_edit(request, period, pk):
         return redirect('ogevents.views.event_list_past')
   else:
     form = EventForm(instance=event)
-  return render(request, 'ogevents/event_edit.html', {'form': form})
+  return render(request, 'ogevents/event_input_form.html', {'form': form})
 
 @login_required
 def event_remove(request, period, pk):
   event = get_object_or_404(Event, pk=pk)
   
   user                           = User.objects.get(id=request.user.id)
-  userdetail                     = user.userdetail
+  #userdetail                     = user.userdetail
 
   if user.is_superuser           == True:
     event.delete()
-  elif event.author              == userdetail:
+  elif event.author              == user:
     event.delete()
-  elif event.schememanager       == userdetail:
+  elif event.schememanager       == user:
     event.delete()
   else:
     pass
